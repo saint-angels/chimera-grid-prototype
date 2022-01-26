867607cc254702af54db365ed0108231dd604305
michael <you@example.com>
2022-01-26 15:50:15 +0300

    init

---
+++ Assets/Scripts/Helpers/ObjectPool/PoolMember.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Backgammon.Helpers.ObjectPool
+{
+    // Added to freshly instantiated objects, to link to the correct pool on despawn.
+    public class PoolMember : MonoBehaviour
+    {
+        public ObjectPool.Pool myPool;
+    }
+}
